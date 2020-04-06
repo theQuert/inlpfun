3ef0c1afac5eda546e3eaea61ef4622e3870b6af
Quert <a97041304@gmail.com>
2020-04-06 15:10:48 +0800

    Add word2vec

---
+++ word2vec/wiki_to_txt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 14:33:36 2020
+
+@author: quert
+"""
+
+import logging
+import sys
+
+from gensim.corpora import WikiCorpus
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 " + sys.argv[0] + " wiki_data_path")
+        exit()
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    wiki_corpus = WikiCorpus(sys.argv[1], dictionary={})
+    texts_num = 0
+
+    with open("wiki_texts.txt",'w',encoding='utf-8') as output:
+        for text in wiki_corpus.get_texts():
+            output.write(' '.join(text) + '\n')
+            texts_num += 1
+            if texts_num % 10000 == 0:
+                logging.info("已處理 %d 篇文章" % texts_num)
+
+if __name__ == "__main__":
+    main()
