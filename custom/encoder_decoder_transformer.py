cec4b19acb7030d3b9171c098544315d7ef13936
Quert <a97041304@gmail.com>
2020-10-26 19:20:55 +0800

    Add "nn.Embedding" to code

--- custom/encoder_decoder_transformer.py
+++ custom/encoder_decoder_transformer.py
@@ -1,3 +1,14 @@
+# Build the lookup table (Embeddings)
+# nn.Embeddings(num_embedding, embedding_dim)
+class Embeddings(nn.Module):
+	def __init__(self, d_model, vocab):
+		super(Embeddings, self).__init__()
+		self.lut = Embedding(vocab, d_model)
+		self.d_model = d_model
+
+	def forward(self, x):
+		return self.lut(x) * math.sqrt(self.d_model)
+
 class EncoderDecoder(nn.Moddule):
 	def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
 		super(EncoderDecoder, self).__init__()
@@ -26,11 +37,11 @@ class Generator(nn.Module):
 	def forward(self, x):
 		return F.log_softmax(self.proj(x), dim=1)
 
-
-# Encoder
 def clones(module, N):
 	"Produce N idendical layers."
 	return nn.ModuleList([copy.deepcopy(module)] for _ in range(N))
+
+# Encoder
 
 class Encoder(nn.Module):
 	def __init__(self, layer, N):
@@ -81,6 +92,77 @@ class EncoderLayer(nn.Module):
 		# 2 sublayers
 		self.sublayer = clones(SublayerConnection(size, dropout), 2)
 		self.size = size
+	'''
+		Attention and Feed_forward
+	'''
 
 	def forward(self, x, mask):
-		x = self.sublayer[0]
+		x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+		return self.sublayer[1](x, self.feed_forward)
+
+# Decoder
+	'''
+		Decoder is composed of a stack of N = 6 identical layers
+	'''
+	def Decoder(nn.Module):
+		def __init__(self, layer, N):
+			super(Decoder, self).__init__()
+			self.layers = clones(layer, N)
+			self.norm = LayerNorm(layer.size)
+		
+		def forward(self, x, memory, src_mask, tgt_mask):
+			for layer in self.layers:
+				x = layer(x, memory, src_mask, tgt_mask)
+			return self.norm(x)
+
+	'''
+		In addition to the two sub-layers in each encoder layer, the decoder
+		inserts a third sub-layer, which performs multi-head attention over 
+		the output of the encoder stack.
+
+		Similar to the encoder, we also employ residual connection around each
+		of the sub-layers, followed the layer norm.
+	'''
+	
+	class DecoderLayer(nn.Module):
+		'''
+			Decoder includes self-attn, src-attn, and feed forward
+		'''
+		def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+			super(DecoderLayer, self).__init__()
+			self.size = size
+			self.self_attn = self_attn
+			self.src_attn = src_attn
+			self.feed_forward = feed_forward
+			self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+		def forward(self, x, memory, src_mask, tgt_mask):
+			m = memory
+			x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+			x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+			return self.sublayer[2](x, self.feed_forward)
+
+		''' 
+			Prevent positions from attending to subsequent positions
+			"i" can only depend on the known outputs at positions less than "i"
+		'''
+		def subsequent_mask(size):
+			attn_shape = (1, size, size)
+			subsequent_mask = np.triu(np.ones(attn_shape), k = 1).astype('uint8')
+			return torch.from_numpy(subsequent_mask) == 0
+
+# Attention
+def attention(query, key, value, mask=None, dropout=None):
+	''' 
+		Compute "Scaled Dot Product Attention"
+	'''
+	d_k = query.size(-1)
+	scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+	if mask is not None:
+		scores = scores.masked_fill(mask == 0, -1e9)
+
+	p_attn = F.softmax(scores, dim = -1)
+
+	if dropout is not None:
+		p_attn = dropout(p_attn)
+
